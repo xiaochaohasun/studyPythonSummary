ea09a82825ed8ab3dbff994d78a8be8d5ae49191
xiaochaohasun@163.com <xiaochaohasun@163.com>
2017-09-19 15:21:11 +0800

    tab.py add help info

--- tab.py
+++ tab.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import readline
 import rlcompleter
 
 readline.parse_and_bind('tab: complete')
 
+#1.查看python  sys.path
+#python
+#>>>import sys
+#>>>sys.path
 
+#2.编辑tab.py后放入sys.path任意一个变量中
+
+#3.加入全局变量
+#vim /etc/profile
+#export PATHSTATRTUP=/path/tab.py
